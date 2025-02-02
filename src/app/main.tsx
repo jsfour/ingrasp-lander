a421c7bfd5bea55e54408e69f67ae8690d2a71fb
James Smoot <jimi@jsfour.com>
2025-02-02 11:42:48 -0800

    drop section

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -1,24 +1,20 @@
 "use client";
 import * as React from "react";
 import HeroSection from "./HeroSection";
-import ProblemSection from "./ProblemSection";
 import ProductSection from "./ProductSection";
 import DifferentiatorsSection from "./DifferentiatorsSection";
 import { NormalPeopleCarousel } from './components/NormalPeopleCarousel';
 import LogoStrip from './components/LogoStrip';
 import TestamonialSection from './components/TestamonialSection';
-import ProblemStatement from './components/ProblemStatement';
 import BookDemo from './components/BookDemo';
 import MainLayout from './components/MainLayout';
 import { demoLink } from './constants/link-urls';
-import Faq from "./components/Faq";
 
 export default function Main() {
   return (
     <MainLayout>
       <HeroSection />
       <LogoStrip />
-      <ProblemStatement />
       <DifferentiatorsSection />
       <TestamonialSection
         quote="We used to pay $800 for just the photographer on a single, short photoshoot. Now I can build my own scenes anytime."
